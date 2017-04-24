515a89c683627dbdf73d93d82fe68b64531660f1
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-24 12:22:23 -0700

    modified classes to work w/ db

--- src/main/java/tay/api/Nutrition.java
+++ src/main/java/tay/api/Nutrition.java
@@ -1,14 +1,40 @@
 package tay.api;
 
+import javax.persistence.*;
+
 /**
  * Created by yoseph on 4/22/17.
  */
+
+@Entity
+@Table(name="nutrition")
+@NamedQueries({
+        @NamedQuery(name = "tay.core.Nutrition.findAll",
+        query = "select n from Nutrition n")
+//        @NamedQuery(name = "tay.core.Nutrition.findByName",
+//        query = "select n from Nutrition where n.type like :type")
+})
+
 public class Nutrition {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
     private String type;
     private int calories;
     private int protein;
     private int fiber;
+
+    public Nutrition() {
+    }
+
+    public Nutrition(String type, int calories, int protein, int fiber) {
+        this.type = type;
+        this.calories = calories;
+        this.protein = protein;
+        this.fiber = fiber;
+    }
 
     public String getType() {
         return type;
