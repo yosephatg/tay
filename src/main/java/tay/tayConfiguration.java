ffa87aa3a2324579200f1baa30ce162c7b45e0cf
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-24 12:04:54 -0700

    change config file

--- src/main/java/tay/tayConfiguration.java
+++ src/main/java/tay/tayConfiguration.java
@@ -2,14 +2,25 @@ package tay;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class tayConfiguration extends Configuration {
-    // TODO: implement service configuration
 
     @NotEmpty
     private String dateFormat;
+
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
 
     public String getDateFormat() {
         return dateFormat;
