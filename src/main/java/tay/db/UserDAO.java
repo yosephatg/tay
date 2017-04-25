ed77bf9dc6795010728e7a46081caa78d9d2cc3a
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-25 17:04:53 -0400

    userDao cleanup

--- src/main/java/tay/db/UserDAO.java
+++ src/main/java/tay/db/UserDAO.java
@@ -1,10 +1,13 @@
 package tay.db;
 
 import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import tay.api.User;
 
+import javax.ws.rs.NotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +22,31 @@ public class UserDAO extends AbstractDAO<User>{
 
     public User findById(Long userId) {
         User user = get(userId);
+        if (user == null) {
+            throw new NotFoundException();
+        }
+        return user;
     }
+
+    public User add(???){
+
+    }
+
+    public void update(User user) {
+        persist(user);
+    }
+
+    public Optional<User>findByUsername(String username) {
+        Criteria criteria = currentSession().createCriteria(User.class);
+        criteria.add(Restrictions.eq("username", username).ignoreCase());
+        List<User> users = list(criteria);
+        if (users.size() == 1) {
+            return Optional.of(users.get(0));
+        } else {
+            return Optional.empty();
+        }
+    }
+
     public Optional<User> findByUsernameAndPassword(String username, String password) {
         return (Optional<User>) currentSession()
                 .createQuery("select u from User u where u.username = :username and u.password = :password")
