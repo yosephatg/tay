c2840f6a948c0ea68ec1fdca7d996113049f5930
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-25 17:55:59 -0400

    authtoken class

--- src/main/java/tay/api/AuthToken.java
+++ src/main/java/tay/api/AuthToken.java
@@ -1,7 +1,58 @@
 package tay.api;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Created by yoseph on 4/25/17.
  */
-public class AuthToken {
+
+@Entity
+@Table(name = "auth_tokens")
+public class AuthToken implements Serializable{
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "created_at", insertable = false, nullable = false, updatable = false)
+    private Date createdAt;
+
+    private String token;
+
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
