a9af8235a9e6629469fc84f87ef781a158e14eb5
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-25 17:10:43 -0400

    skeleton for user interactions

--- src/main/java/tay/core/TayCore.java
+++ src/main/java/tay/core/TayCore.java
@@ -1,7 +1,56 @@
 package tay.core;
 
+import org.hibernate.SessionFactory;
+import tay.api.Nutrition;
+import tay.api.User;
+import tay.db.NutritionDAO;
+import tay.db.UserDAO;
+
+import java.util.Optional;
+
 /**
  * Created by yoseph on 4/25/17.
  */
 public class TayCore {
+
+    private final SessionFactory sessionFactory;
+    private final UserDAO userDAO;
+    private final NutritionDAO nutritionDAO;
+
+    public TayCore(SessionFactory sessionFactory, UserDAO userDAO, NutritionDAO nutritionDAO) {
+        this.sessionFactory = sessionFactory;
+        this.userDAO = userDAO;
+        this.nutritionDAO = nutritionDAO;
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    // USER
+
+    public User addUser(???) {
+        // get info from passed parameter
+        // call userDao.add
+    }
+
+    public User update(???) {
+        // get info from passed parameter
+        // call userDao.update
+    }
+
+    public void changePassword(???) {
+        // get info from passed parameter
+        // call userDao.update
+    }
+
+    public Optional<User> findUserByToken(String token) {
+        // use token parameter, call authTokenDao.find, return user
+    }
+
+    public Optional<User> login(???) {
+        // passed in parameter should have username, password, token
+        // confirm they're accurate and return user
+    }
+
 }
