8168173c7f3b3366317d06c5c1d72d30c772f70e
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-25 17:16:34 -0400

    refactored

--- src/main/java/tay/tayApplication.java
+++ src/main/java/tay/tayApplication.java
@@ -1,13 +1,20 @@
 package tay;
 
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import tay.api.Nutrition;
+import tay.api.User;
+import tay.auth.UserAuth;
 import tay.core.NutritionRepository;
+import tay.core.TayCore;
 import tay.db.NutritionDAO;
+import tay.db.UserDAO;
 import tay.resource.EventResource;
 import tay.resource.NutritionResource;
 
@@ -40,18 +47,31 @@ public class tayApplication extends Application<tayConfiguration> {
     @Override
     public void run(final tayConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
-        DateFormat testDateFormat = new SimpleDateFormat(configuration.getDateFormat());
-        environment.getObjectMapper().setDateFormat(testDateFormat);
 
+        // DAO
         final NutritionDAO nutritionDAO = new NutritionDAO(hibernateBundle.getSessionFactory());
+        final UserDAO userDAO = new UserDAO(hibernateBundle.getSessionFactory());
 
+        // Core
+        final TayCore tayCore = new TayCore(hibernateBundle.getSessionFactory(), userDAO, nutritionDAO);
+
+        // Resource
         NutritionResource nutritionResource = new NutritionResource(nutritionDAO);
         environment.jersey().register(nutritionResource);
-
         EventResource eventResource = new EventResource();
         environment.jersey().register(eventResource);
 
+
+        // Misc, figure out where these go!
+        DateFormat testDateFormat = new SimpleDateFormat(configuration.getDateFormat());
+        environment.getObjectMapper().setDateFormat(testDateFormat);
+
+        environment.jersey().register(new AuthDynamicFeature(
+                new BasicCredentialAuthFilter.Builder<User>()
+                .setAuthenticator(new UserAuth(userDAO))
+                .setRealm("Test")
+                .buildAuthFilter()
+        ));
 
     }
 
