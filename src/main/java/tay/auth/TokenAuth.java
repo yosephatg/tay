3847587bf52b4e096716d59d5d3316c9b4fb6e70
Yoseph Tesfagaber <joseph.ahferom@gmail.com>
2017-04-25 17:38:10 -0400

    Token auth

---
+++ src/main/java/tay/auth/TokenAuth.java
@@ -0,0 +1,30 @@
+package tay.auth;
+
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import tay.api.User;
+import tay.core.TayCore;
+
+import java.util.Optional;
+
+/**
+ * Created by yoseph on 4/25/17.
+ */
+public class TokenAuth implements Authenticator<String, User> {
+    private final TayCore tayCore;
+
+    public TokenAuth(TayCore tayCore) {
+        this.tayCore = tayCore;
+    }
+
+
+    @Override
+    public Optional<User> authenticate(String token) throws AuthenticationException {
+        java.util.Optional<User> possibleUser = tayCore.findUserByToken(token);
+        if (possibleUser.isPresent()) {
+            return Optional.of(possibleUser.get());
+        } else {
+            return Optional.empty();
+        }
+    }
+}
